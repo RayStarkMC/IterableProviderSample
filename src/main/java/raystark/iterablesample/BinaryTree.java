84ee1f5ed4cebd82b1c699406b81787c542b74a7
RayStarkMC <raystarkmc@gmail.com>
2020-12-17 20:42:22 +0900

    Iterableを生成する方式から委譲方式に変更

--- src/main/java/raystark/iterablesample/BinaryTree.java
+++ src/main/java/raystark/iterablesample/BinaryTree.java
@@ -6,8 +6,17 @@ import raystark.eflib.option.Option;
 public final class BinaryTree<T> {
     private final Option<Node<T>> root;
 
+    private final Iterable<T> preorder;
+    private final Iterable<T> inorder;
+    private final Iterable<T> postorder;
+    private final Iterable<T> breadthFirst;
+
     public BinaryTree(@NotNull Option<Node<T>> root) {
         this.root = root;
+        this.preorder = () -> new PreorderIterator<>(this);
+        this.inorder = () -> new InorderIterator<>(this);
+        this.postorder = () -> new PostorderIterator<>(this);
+        this.breadthFirst = () -> new BreadthFirstIterator<>(this);
     }
 
     public @NotNull Option<Node<T>> root() {
@@ -15,18 +24,18 @@ public final class BinaryTree<T> {
     }
 
     public @NotNull Iterable<T> preorderIterable() {
-        return () -> new PreorderIterator<>(this);
+        return preorder;
     }
 
     public @NotNull Iterable<T> inorderIterable() {
-        return () -> new InorderIterator<>(this);
+        return inorder;
     }
 
     public @NotNull Iterable<T> postorderIterable() {
-        return () -> new PostorderIterator<>(this);
+        return postorder;
     }
 
     public @NotNull Iterable<T> breadthFirstIterable() {
-        return () -> new BreadthFirstIterator<>(this);
+        return breadthFirst;
     }
 }
